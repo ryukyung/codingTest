26a8d8ae79a5c48e6f9c3366131672eb73515c54
ryukyunghye <ryukyung123@naver.com>
2023-02-27 15:40:14 +0900

    :sparkles: (week01/#13): 옷가게 할인 받기

---
+++ week01/옷가게 할인 받기.js
@@ -0,0 +1,7 @@
+// 옷가게 할인 받기
+function solution(price) {
+  if (price >= 500000) price *= 0.8;
+  else if (price >= 300000) price *= 0.9;
+  else if (price >= 100000) price *= 0.95;
+  return parseInt(price);
+}
